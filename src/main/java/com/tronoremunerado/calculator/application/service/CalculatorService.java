502d3e732beff84f7096a2a898f66c978fbe2ffd
JoaoMattheus <jaomatth@gmail.com>
2025-08-20 08:36:25 -0300

    refactor CalculatorService to improve salary calculation logic and enhance entity mapping

--- src/main/java/com/tronoremunerado/calculator/application/service/CalculatorService.java
+++ src/main/java/com/tronoremunerado/calculator/application/service/CalculatorService.java
@@ -1,20 +1,34 @@
 package com.tronoremunerado.calculator.application.service;
 
 import com.tronoremunerado.calculator.application.mapper.CalculationResponseMapper;
+import com.tronoremunerado.calculator.application.mapper.KingEntityMapper;
 import com.tronoremunerado.calculator.application.ports.input.CalculateSalaryUseCase;
 import com.tronoremunerado.calculator.domain.King;
 import com.tronoremunerado.calculator.domain.KingCalculateResponse;
+import com.tronoremunerado.calculator.domain.KingEntity;
+import com.tronoremunerado.calculator.infrastructure.input.rest.KingDBConnectionUseInCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
 public class CalculatorService implements CalculateSalaryUseCase {
 
-    private final CalculationResponseMapper mapper;
+    private final CalculationResponseMapper mapperKingResponse;
+    private final KingEntityMapper mapperKingEntity;
+    private final KingDBConnectionUseInCase kingDBConnection;
 
     @Override
     public KingCalculateResponse calculateSalary(King king) {
-        return mapper.toResponse(king);
+
+        KingCalculateResponse kingResponse = mapperKingResponse.toResponse(king);
+        KingEntity kingEntity = mapperKingEntity.toEntity(kingResponse);
+
+        CompletableFuture.runAsync(() -> {
+            kingDBConnection.saveKing(kingEntity);
+        });
+        return kingResponse;
     }
 }
